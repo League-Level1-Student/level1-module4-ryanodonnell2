e37406e674cc42cbece9538565a42bfa0d851f3f
O'Donnell.Ryan <O'Donnell.Ryan@SEMS-259-32.SEMS259.LAB>
2019-02-20 16:30:28 -0800

    Chuckle  Clicker Typing Tutor

--- src/SimonSays.java
+++ src/SimonSays.java
@@ -26,68 +26,95 @@ public class SimonSays extends KeyAdapter {
 	HashMap<Integer, String> images = new HashMap<Integer, String>();
 	private int imageIndex;
 	private int tries = 0;
+	int points = 0;
 	private boolean simonSays = false;
 	Date timeAtStart;
 
 	private void makeAlbum() {
 		// 2. Add the four images that match keyboard keys like this: 
 		//images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
-
+		images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
+		images.put(new Integer(KeyEvent.VK_DOWN), "down.jpg");
+		images.put(new Integer(KeyEvent.VK_LEFT), "left.jpg");
+		images.put(new Integer(KeyEvent.VK_RIGHT), "right.jpg");
 		// 3. Use a JOptionPane to tell the user the rules: "Press the matching key when
 		// 'Simon says' otherwise press a different key"
-		
+		JOptionPane.showMessageDialog(null, "Press the matching key when 'Simon says' otherwise press a different key");
 		// 4. Call the showImage method to show an image
+		showImage();
 	}
 
 	public void keyPressed(KeyEvent e) {
+
 		// 15. Make a points variable to track the score.
+		int keyCode = e.getKeyCode();
 
 		// 16. If the keyCode matches the imageIndex and "Simon says"
-		
+		if(keyCode == imageIndex && simonSays == true) {
 			// 17. Increase the value of score
-		
+			points++;
 			// 18. Use the speak method to tell the user they were correct
-		
+			speak("Correct!");
+			tries++;
+		}
 		// 19. If the keyCode doesn't match the imageIndex and "Simon didn't say..."
-		
+		else if(keyCode != imageIndex && simonSays == false){
 			// 20.  Increase the value of score
-		
+			points++;
 			// 21. Use the speak method to tell the user they were correct
-		
+			speak("Correct!");
+			tries++;
+		}
+		else {
+
+			// 21. Use the speak method to tell the user they were correct
+			speak("Incorrect!");
 		// 22. Increment tries by 1
-		
+			tries++;
+		}
 		// 25. If tries is greater than 9 (or however many you want)...
-		
+		if(tries > 249) {
 			// 26. Tell the user their score
+			System.out.println("Your Score is " + points + "!");
 		
 			// 27. Exit the program
-
+		System.exit(0);
+		}
 		// 23. Dispose of the frame
-
+		frame.dispose();
+		
 		// 24. Call the showImage method to show a new image
+		showImage();
 	}
 
 	private void showImage() {
 		// 5. Initialize your frame to a new JFrame()
-		
+		frame = new JFrame();
 		// 6. Set the frame to visible
-
+			frame.setVisible(true);
 		// 7. Uncomment the following line to add a random image to your frame
-		//frame.add(getNextRandomImage());
+		frame.add(getNextRandomImage());
 
 		// 8. Set the name of your frame
-
+		
 		// 9. Pack the frame
-		
+		frame.pack();
 		// 10. Set the defaultCloseOperation of your from to JFrame.EXIT_ON_CLOSE
-		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 11. Add a key listener to the frame
-
+		frame.addKeyListener(this);
 		// 12. Create a new instance of Random
-
+		Random randy = new Random();
 		// 13. Use the Random and the speak method to either say 
 		// "Simon says press this key" or "Press this key"
 
+		simonSays = randy.nextBoolean();
+		if(simonSays == true) {
+			speak("Simon says press this key");
+		}
+		else {
+		speak("press this key");
+		}
 		// 14. Above, set the value of simonSays to true/false appropriately
 
 		
@@ -108,7 +135,8 @@ public class SimonSays extends KeyAdapter {
 		try {
 			Runtime.getRuntime().exec("say " + words).waitFor();
 		} catch (Exception e) {
-			e.printStackTrace();
+			//e.printStackTrace();
+			System.out.println(words);
 		}
 	}
 
